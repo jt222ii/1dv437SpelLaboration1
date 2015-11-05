32d823b8690ddc4f8bc901526c4a176d89736002
jt222ii <jt222ii@student.lnu.se>
2015-11-05 16:44:25 +0100

    moved a duplicated variable to a field

--- Laboration1MV-transformationer/Laboration1MV-transformationer/Camera.cs
+++ Laboration1MV-transformationer/Laboration1MV-transformationer/Camera.cs
@@ -10,16 +10,18 @@ namespace Laboration1MV_transformationer
     {
         private int sizeOfTile = 64;
         private int borderSize = 64;
+        private int visualX;
+        private int visualY;
         public Vector2 getVisualCoords(int xCoord, int yCoord)
         {
-            int visualX = borderSize + xCoord * sizeOfTile;
-            int visualY = borderSize + yCoord * sizeOfTile;
+            visualX = borderSize + xCoord * sizeOfTile;
+            visualY = borderSize + yCoord * sizeOfTile;
             return new Vector2(visualX, visualY);
         }
         public Vector2 rotateView(int xCoord, int yCoord)
         {
-            int visualX = sizeOfTile*8 - (xCoord * sizeOfTile);
-            int visualY = sizeOfTile*8 - (yCoord * sizeOfTile);
+            visualX = sizeOfTile*8 - (xCoord * sizeOfTile);
+            visualY = sizeOfTile*8 - (yCoord * sizeOfTile);
             return new Vector2(visualX, visualY);
         }
     }
